6ba29e10092f42e5fb15e7c1b98ba395c037da4f
Pretty Han <imbel9830@gmail.com>
2022-01-18 14:49:27 +0900

    1st

---
+++ 22.01.18/1.js
@@ -0,0 +1,10 @@
+var num;
+num = 1;
+
+var str = "Hello World";
+str = "Nice to meet you";
+console.log(num);
+console.log(str);
+
+document.write(num);
+document.write(str);
---
+++ 22.01.18/배열.js
@@ -0,0 +1,7 @@
+var fruit = ["Apple","Banana"];
+document.write(fruit);
+document.write(fruit[0]);
+
+fruit[0] ="Tomato";
+document.write(fruit);
+
