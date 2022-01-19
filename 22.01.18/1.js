a1510b0494a82cf9b22bd37f57092b54021d1d2b
Pretty Han <imbel9830@gmail.com>
2022-01-19 10:50:54 +0900

    1st

--- 22.01.18/1.js
+++ 22.01.18/1.js
@@ -1,10 +1,13 @@
-var num;
-num = 1;
+// 훈련병들이 있는 배열입니다.
+var soldier = [12, 2, 5, 3, 7, 4, 10, 8, 1, 9, 13, 11, 6];
 
-var str = "Hello World";
-str = "Nice to meet you";
-console.log(num);
-console.log(str);
+// 정렬된 배열과 길이를 구하세요.
+function compareNumbers(a, b) {
+    return a - b;
+}
+solidier.compareNumbers 
+var count = 0;
 
-document.write(num);
-document.write(str);
+// 채점을 위한 코드입니다. 수정 하지 마세요!
+console.log(soldier);
+console.log(count);
