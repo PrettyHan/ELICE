2748923ba1430a02acdba38a09f64d66b7a25444
Pretty Han <imbel9830@gmail.com>
2022-02-10 17:11:03 +0900

    1st

---
+++ 22.02.10/Promice.js
@@ -0,0 +1,18 @@
+function timer(time){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(time);
+        }, time);
+    });
+}
+
+timer(1000).then(function(time){
+    console.log('time:'+time);
+    return timer(time+1000);
+}).then(function(time){
+    console.log('time:'+time);
+    return timer(time+1000);
+}).then(function(time){
+    console.log('time:'+time);
+    console.timeEnd('timer');
+})
