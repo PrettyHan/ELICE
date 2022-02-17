92e2fa54f1e333d236245807a5cf37fcda931466
PrettyHan <imbel9830@gmail.com>
2022-02-17 10:47:09 +0900

    npm start

---
+++ 22.02.17/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res) => {
+        res.send('hello, world')
+})
+
+// req : request(요청), 사용자의 브라우저 정보, 질문(주소창), 로그인정보
+// res : response(응답), 사이트 내용 html
+
+app.listen(3000, () => {
+        console.log('3000번 포트에서 웹서버를 실행중 입니다...')
+})
