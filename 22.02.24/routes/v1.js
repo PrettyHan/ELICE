98f4b3b8bbe8993d5f62bb106fbfce9e60925f45
PrettyHan <imbel9830@gmail.com>
2022-02-24 14:57:07 +0900

    1st

--- 22.02.24/routes/v1.js
+++ 22.02.24/routes/v1.js
@@ -1,22 +1,37 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const router = express.Router()
 
 const { verifyToken } = require('./middleware.js')
 
-router.get('/token', async (req,res) => {
+const router = express.Router()
+
+router.get('/token', async (req, res) => {
     try {
         const token = jwt.sign({
-            id : '사용자1',
-            nick : '사용자nick',
-            grade: 'gold',
-
-        }, process.env.jwt_SECRET, {
-            expiresIn: '10m',
-            issuer:'mySNS'
+            id: '내sns사용자',
+            nick: 'consumer88',
+            grade: 'premium'
+        }, process.env.JWT_SECRET, {
+            expiresIn: '60m',
+            issuer: 'mySNS'
         })
-
+        req.session.jwt = token
+        return res.json({
+            code: 200,
+            message: '토큰이 발급되었습니다',
+            token
+        })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러'
+        })
     }
 })
 
-router.get('/test')
+router.get('/test', verifyToken, (req, res)=> {
+    res.json(req.decoded)
+})
+
+module.exports = router
