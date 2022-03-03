fb4040b5a1878890523f425f4532645bed4459fb
PrettyHan <imbel9830@gmail.com>
2022-03-03 14:54:36 +0900

    2nd

--- 22.03.03/my-app/src/App.js
+++ 22.03.03/my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Link, Routes, Route, useParams} from 'react-router-dom';
+import {Link, Routes, Route, useParams, useNavigate} from 'react-router-dom';
 import {useState} from 'react';
 function Article(props){
   return <article>
@@ -37,27 +37,100 @@ function Read(props){
   }
   return <Article title={title} body={body}></Article>
 }
+function Create(props){
+  function submitHandler(event){
+    event.preventDefault();
+    let t = event.target.title.value;
+    let b = event.target.body.value;
+    props.onCreate(t, b);
+  }
+  return <article>
+    <h1>Create</h1>
+    <form onSubmit={submitHandler}>
+      <p><input type="text" name="title" placeholder="Title"></input></p>
+      <p><textarea name="body" placeholder="Body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+function Update(props){
+  const params = useParams();
+  const id = Number(params.id);
+  let _title, _body = '';
+  for(let i=0; i<props.topics.length; i++){
+    if(props.topics[i].id === id){
+      _title = props.topics[i].title;
+      _body = props.topics[i].body;
+    }
+  }
+  const [title, setTitle] = useState(_title);
+  const [body, setBody] = useState(_body);
+  function submitHandler(event){
+    event.preventDefault();
+    let t = event.target.title.value;
+    let b = event.target.body.value;
+    props.onUpdate(id, t, b);
+  }
+  return <article>
+    <h1>Update</h1>
+    <form onSubmit={submitHandler}>
+      <p><input type="text" name="title" placeholder="Title" value={title} onChange={event=>setTitle(event.target.value)}></input></p>
+      <p><textarea name="body" placeholder="Body" value={body} onChange={event=>setBody(event.target.value)}></textarea></p>
+      <p><input type="submit" value="Update"></input></p>
+    </form>
+  </article>
+}
 function App() {
   console.log('App render');
-  let topics = [
+  const navigate = useNavigate();
+  let [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'}
-  ];
-  const [id, setId] = useState(null);
+  ]);
+  let [nextId, setNextId] = useState(4);
+  function createHandler(_title, _body){
+    let newTopics = [...topics];
+    const newTopic = {id:nextId, title:_title, body:_body};
+    newTopics.push(newTopic);
+    setTopics(newTopics);
+    navigate('/read/'+nextId);
+    setNextId(nextId+1);
+  }
+  function updateHandler(_id, _title, _body){
+    let newTopics = [...topics];
+    for(let i=0; i<newTopics.length; i++){
+      if(newTopics[i].id === _id){
+        newTopics[i].title = _title;
+        newTopics[i].body = _body;
+      }
+    }
+    setTopics(newTopics);
+    navigate('/read/'+_id);
+  }
   return <>
       <Header title="React"></Header>
       <Nav topics={topics}></Nav>
       <Routes>
         <Route path="/" element={<Article title="Welcome" body="Hello, WEB"></Article>}></Route>
         <Route path="/read/:id" element={<Read topics={topics}></Read>}></Route>
+        <Route path="/create" element={<Create onCreate={createHandler}></Create>}></Route>
+        <Route path="/update/:id" element={<Update topics={topics} onUpdate={updateHandler}></Update>}></Route>
       </Routes>
-      <ul>
-        <li><a href="/create">Create</a></li>
-        <li><a href="/update">Update</a></li>
-        <li><input type="button" value="Delete" /></li>
-      </ul>
+      <Routes>
+        <Route path="/" element={<Control></Control>}></Route>
+        <Route path="/read/:id" element={<Control></Control>}></Route>
+      </Routes>
     </>
+}
+function Control(){
+  const params = useParams();
+  console.log('params', params);
+  return <ul>
+    <li><Link to="/create">Create</Link></li>
+    <li><Link to={'/update/'+params.id}>Update</Link></li>
+    <li><input type="button" value="Delete" /></li>
+  </ul> 
 }
 
 export default App;
